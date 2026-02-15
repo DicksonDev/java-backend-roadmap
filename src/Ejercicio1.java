cd371be70f256f2c5127d051c6234c2385d0e896
Dickson <dicksonvejardev@gmail.com>
2026-02-15 17:03:56 -0400

    02/15/26 ejericio Condificonal if / if else / While / metodo random de la clase math

---
+++ src/Ejercicio1.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int minvalue = 1;
+        int maxvalue = 100;
+        int intentos = 0;
+        int randomnumber = (int)(Math.random() * (maxvalue - minvalue + 1) + minvalue);
+        boolean iscorrect = false;
+        System.out.println(randomnumber);
+        System.out.println("--Adivina el numero--!");
+        System.out.println("Ingresa el numero");
+        while(!iscorrect) {
+            int entradaUsuario = sc.nextInt();
+
+            if (entradaUsuario == randomnumber) {
+                System.out.println("Adivinaste el numero, Felicidades");
+                System.out.println("El numero de intentos fue de " + intentos);
+                iscorrect = true;
+            } else if (entradaUsuario > randomnumber) {
+                System.out.println("El numero ingresado es mayor al que tratas de adivinar");
+                System.out.println("Intentalo nuevamente");
+                intentos++;
+            } else {
+                System.out.println("El numero ingresado es menor al que tratas de adivinar");
+                System.out.println("Intentalo nuevamente");
+                intentos++;
+
+            }
+        }
+
+
+    }
+}
